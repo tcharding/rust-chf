d583ac8e90f3bcabc6014be23a50398f7fce9ae2
Tobin C. Harding <me@tobin.cc>
2024-05-03 08:32:24 +1000

    CI: Add workflow
    Use `rust-bitcoin-maintainer-tools` and add a workflow.

--- contrib/test_vars.sh
+++ contrib/test_vars.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 # Test all these features with "std" enabled.
-FEATURES_WITH_STD="io serde small-hash schemars"
+FEATURES_WITH_STD="serde small-hash schemars"
 
 # Test all these features without "std" enabled.
-FEATURES_WITHOUT_STD="alloc serde small-hash"
-
-# Run address sanitizer with these features.
-ASAN_FEATURES="std io serde"
+FEATURES_WITHOUT_STD="io serde small-hash schemars"
 
 # Run and lint these examples.
 EXAMPLES=""
